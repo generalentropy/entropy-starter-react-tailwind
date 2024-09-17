e0afd22e34c11076e5f6ec58d0501480346f0ac1
entropie <entropie@tuta.io>
2024-09-17 13:31:09 +0200

    update

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,14 @@
+import notFoundImage from "../assets/images/notfound.webp";
+
 function NotFound() {
   return (
     <div className="flex h-screen items-center justify-center bg-gray-800">
-      <div className="flex flex-col text-center text-pink-300">
-        <h1 className="mb-2 text-4xl font-bold">404 - Page Not Found</h1>
-        <p>Sorry, the page you are looking for could not be found.</p>
+      <div className="flex flex-col text-center text-fuchsia-200">
+        <img src={notFoundImage} />
+        {/* <h1 className="mb-2 text-4xl font-bold">404 - Page Not Found</h1> */}
+        <p className="mx-2 text-lg">
+          Sorry, the page you are looking for could not be found.
+        </p>
       </div>
     </div>
   );
